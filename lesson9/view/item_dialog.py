dc012149c3ca600f6b02e0e693ecf6144647ae4a
Richard <cmlee3636@gmail.com>
2024-11-13 13:44:05 +0800

    lesson9

--- lesson9/view/item_dialog.py
+++ lesson9/view/item_dialog.py
@@ -1,22 +1,42 @@
 import tkinter as tk
+from tkinter import ttk
 from tkinter.simpledialog import Dialog
 
 
 class MyCustomDialog(Dialog):
-    def __init__(self, parent, title=None):
+    def __init__(self, parent, record: list, title=None):
+        self.date = record[0]
+        self.county = record[1]
+        self.sitename = record[2]
+        self.aqi = record[3]
+        self.pm25 = record[4]
+        self.status = record[5]
+        self.lat = float(record[6])
+        self.lon = float(record[7])
         super().__init__(parent=parent, title=title)
 
     def body(self, master):
         # 創建對話框主體。返回應具有初始焦點的控件。
-        tk.Label(master, text="請輸入你的名字:").grid(row=0)
-        self.name_entry = tk.Entry(master)
-        self.name_entry.grid(row=0, column=1)
-        return self.name_entry
+        main_frame = ttk.Frame(master, borderwidth=1, relief='groove')
+        canvas_left = tk.Canvas(main_frame, width=200, height=200)
+        canvas_left.create_rectangle(
+            10, 10, 190, 190, outline="#9E7A7A", width=2)
+        # canvas_left.create_oval(10, 10, 80, 80, outline="#f11",fill="#1f1", width=2)
+        canvas_left.create_text(100, 40, text=f'AQI:{self.status}', font=(
+            "Helvetica", 24, "bold"), fill='#9E7A7A')
+        canvas_left.pack(side='left')
+
+        canvas_right = tk.Canvas(main_frame, width=200, height=200)
+        canvas_right.create_oval(
+            10, 10, 80, 80, outline="#00896C", fill="#566C73", width=2)
+        canvas_right.create_text(200, 150, text=self.status, font=(
+            "Helvetica", 24, "bold"), fill='#9E7A7A')
+        canvas_right.pack(side='right')
+        main_frame.pack(expand=True, fill='x')
 
     def apply(self):
         # 當用戶按下確定時處理數據
         print("使用者按了apply")
-        self.result = self.name_entry.get()
 
     def buttonbox(self):
         # Add custom buttons (overriding the default buttonbox)
